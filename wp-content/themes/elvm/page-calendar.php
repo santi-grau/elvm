5f7215e4815ccffa2839c1ed856fe23e3d155a8e
santi <santi@codekunstler.com>
2014-06-19 10:07:34 +0200

    calendar dates

--- wp-content/themes/elvm/page-calendar.php
+++ wp-content/themes/elvm/page-calendar.php
@@ -14,7 +14,35 @@
 	<article class="calendar-article" style="position: absolute; left: 276px; top: 0px;">
 		<div class="article-header">
 			<div class="article-header-info">
-				<p class="article-date"><?php the_date(); ?></p>
+				<p class="article-date">
+				<?php
+					$date1 = NULL;
+					$date2 = NULL;
+					if(get_field('date_from')) { $date1 = DateTime::createFromFormat('Ymd',get_field('date_from')); }
+					if(get_field('date_to')) { $date2 = DateTime::createFromFormat('Ymd',get_field('date_to')); } 
+					if(isset($date1) && isset($date2)){
+						if($date1->format('Y') == $date2->format('Y')){
+							echo $date1->format('F d').' - '.$date2->format('F d').', '.$date2->format('Y');
+						}else{
+							echo $date1->format('F d, Y').' - '.$date2->format('F d, Y');
+						}
+					}else{
+						echo $date1->format('F d, Y');
+					}
+					$time1 = NULL;
+					$time2 = NULL;
+					if(get_field('time_from')) { $time1 = substr(get_field('time_from'),0,2).':'.substr(get_field('time_from'),-2); }
+					if(get_field('time_to')) { $time2 = substr(get_field('time_to'),0,2).':'.substr(get_field('time_to'),-2); }
+					if(isset($time1) && isset($time2)){
+						echo '<br />';
+						echo $time1 .'-'. $time2;
+					}else if(isset($time1)){
+						echo '<br />';
+						echo $time1;
+					}
+				?>
+				</p>
+
 			</div>
 			<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
 		</div>
