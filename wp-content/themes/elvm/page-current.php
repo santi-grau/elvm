56b2a58bef4ce670d9764684e054fe2a6bda463f
santi <santi@codekunstler.com>
2014-06-26 13:01:27 +0200

    new press in LV exhibits

--- wp-content/themes/elvm/page-current.php
+++ wp-content/themes/elvm/page-current.php
@@ -1,16 +1,16 @@
 <?php get_header(); ?>
+
 	<?php
 		$pro_query = array(
 			'post_type' => 'exhibits',
 			'posts_per_page' => 1,
-			'order' => 'DESC',
-			'orderby' => 'date'
+			'category_name' => 'current'
 		);
 		$projects = new WP_Query($pro_query);
 		while ( $projects->have_posts() ) : $projects->the_post();
 	?>
 	<?php if(get_field('featured_video')){ ?>
-		<a href="javascript:void(0)" id="feat_video" data-url="<?php the_field('featured_video'); ?>" onload="alert('a');" ><span class="play-img"></span></a>
+		<a href="javascript:void(0)" id="feat_video" data-url="<?php the_field('featured_video'); ?>"><span class="play-img"></span></a>
 	<?php } ?>
 	<h1><?php the_title(); ?></h1>
 	<?php the_content(); ?>
--- wp-content/themes/elvm/page-index.php
+++ wp-content/themes/elvm/page-index.php
@@ -1,7 +1,18 @@
-<?php get_header('home'); ?>
-	<?php 
-		while ( have_posts() ) : the_post();
-		the_content();
-		endwhile;  
+<?php get_header(); ?>
+
+	<?php
+		$pro_query = array(
+			'post_type' => 'exhibits',
+			'posts_per_page' => 1,
+			'category_name' => 'current'
+		);
+		$projects = new WP_Query($pro_query);
+		while ( $projects->have_posts() ) : $projects->the_post();
 	?>
-<?php get_footer(); ?>
+	<?php if(get_field('featured_video')){ ?>
+		<a href="javascript:void(0)" id="feat_video" data-autoplay="true" data-url="<?php the_field('featured_video'); ?>"><span class="play-img"></span></a>
+	<?php } ?>
+	<?php
+		endwhile;
+	?>
+<?php get_footer(); ?>¡
