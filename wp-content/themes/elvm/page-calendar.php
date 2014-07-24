a06f6ee66c09690e42382cff7416077c85c44687
santi <santi.grau.torredemer@gmail.com>
2014-07-24 11:40:16 +0200

    fix dates on calendar

--- wp-content/themes/elvm/page-calendar.php
+++ wp-content/themes/elvm/page-calendar.php
@@ -18,27 +18,29 @@
 				<?php
 					$date1 = NULL;
 					$date2 = NULL;
-					if(get_field('date_from')) { $date1 = DateTime::createFromFormat('Ymd',get_field('date_from')); }
-					if(get_field('date_to')) { $date2 = DateTime::createFromFormat('Ymd',get_field('date_to')); } 
-					if(isset($date1) && isset($date2)){
-						if($date1->format('Y') == $date2->format('Y')){
-							echo $date1->format('F d').' - '.$date2->format('F d').', '.$date2->format('Y');
+					if(get_field('date_from') && get_field('date_to')){ 
+						if(get_field('date_from')) { $date1 = DateTime::createFromFormat('Ymd',get_field('date_from')); }
+						if(get_field('date_to')) { $date2 = DateTime::createFromFormat('Ymd',get_field('date_to')); } 
+						if(isset($date1) && isset($date2)){
+							if($date1->format('Y') == $date2->format('Y')){
+								echo $date1->format('F d').' - '.$date2->format('F d').', '.$date2->format('Y');
+							}else{
+								echo $date1->format('F d, Y').' - '.$date2->format('F d, Y');
+							}
 						}else{
-							echo $date1->format('F d, Y').' - '.$date2->format('F d, Y');
+							echo $date1->format('F d, Y');
 						}
-					}else{
-						echo $date1->format('F d, Y');
-					}
-					$time1 = NULL;
-					$time2 = NULL;
-					if(get_field('time_from')) { $time1 = substr(get_field('time_from'),0,2).':'.substr(get_field('time_from'),-2); }
-					if(get_field('time_to')) { $time2 = substr(get_field('time_to'),0,2).':'.substr(get_field('time_to'),-2); }
-					if(isset($time1) && isset($time2)){
-						echo '<br />';
-						echo $time1 .'-'. $time2;
-					}else if(isset($time1)){
-						echo '<br />';
-						echo $time1;
+						$time1 = NULL;
+						$time2 = NULL;
+						if(get_field('time_from')) { $time1 = substr(get_field('time_from'),0,2).':'.substr(get_field('time_from'),-2); }
+						if(get_field('time_to')) { $time2 = substr(get_field('time_to'),0,2).':'.substr(get_field('time_to'),-2); }
+						if(isset($time1) && isset($time2)){
+							echo '<br />';
+							echo $time1 .'-'. $time2;
+						}else if(isset($time1)){
+							echo '<br />';
+							echo $time1;
+						}
 					}
 				?>
 				</p>
