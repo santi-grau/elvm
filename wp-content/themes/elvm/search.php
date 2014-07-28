e4b76ce188adc151d88cd98c1fbf979e27e8fea2
santi <santi.grau.torredemer@gmail.com>
2014-07-28 11:04:23 +0200

    serach field

--- wp-content/themes/elvm/search.php
+++ wp-content/themes/elvm/search.php
@@ -1,7 +1,9 @@
 <?php get_header(); ?>
 <h1 class="page-title"><?php printf( __( 'Search Results for: %s', 'twentyfourteen' ), get_search_query() ); ?></h1>
 <?php if ( have_posts() ) : ?>
-	<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a><br>
+	<?php while ( have_posts() ) : the_post(); ?>
+		<p><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></p>
+	<?php endwhile; ?>
 <?php else : ?>
 	No results
 <?php endif; ?>
